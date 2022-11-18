2cda6aa46212b07fd177f383fae14775adc71bea
Julian Ćwirko <julian.cwirko@gmail.com>
2022-11-18 01:12:10 +0100

    Update Footer.tsx

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -31,7 +31,7 @@ export const Footer = () => {
           <Text
             as="a"
             color="dappTemplate.color3.base"
-            href="https://github.com/zdevguild"
+            href="https://github.com/xdevguild"
             target="_blank"
             rel="noopener noreferrer nofollow"
           >
